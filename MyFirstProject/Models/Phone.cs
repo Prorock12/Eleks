a1f768ed685a40495216f6855724fdef8d91e377
Bohdan Hlyva <34096751+Prorock12@users.noreply.github.com>
2021-01-14 11:12:48 +0200

    new

---
+++ MyFirstProject/Models/Phone.cs
@@ -0,0 +1,43 @@
+﻿using MyFirstProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Models
+{
+    public class Phone
+    {
+        Operation oper = new Operation();
+        string title;
+        string company;
+        int price;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                oper.SetProperty<string>(ref title, value);
+            }
+        }
+        public string Company
+        {
+            get { return company; }
+            set
+            {
+                oper.SetProperty<string>(ref company, value);
+            }
+        }
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                oper.SetProperty<int>(ref price, value);
+            }
+        }
+    }
+}
--- MyFirstProject/Views/MainWindow.xaml.cs
+++ MyFirstProject/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFirstProject.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace MyFirstProject
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
         }
     }
 }
